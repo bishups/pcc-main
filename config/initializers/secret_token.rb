c6042fa3847ad4719b254bda30add7af5782fd06
Abhisek <abhisek@ADs-MacBook-Pro.local>
2013-12-10 22:17:20 +0530

    Project skeleton
    Signed-off-by: Abhisek <abhisek@ADs-MacBook-Pro.local>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+# XXX: This must be re-generated post deployment.
+PccMain::Application.config.secret_token = '9f4944bce06e52241f832e5a70c21d4579cde023af715cd061a2b1505cea05339ff49d1d84d29c6ef695152848f2200f12394effc15843418108af1fda2977f7'
