56066d8cdbb4b5a44f1f9f5641a753e2d2a17b75
Senthil Kumar <senthilkumar@kreatio.com>
2014-01-13 23:27:08 +0530

    Added annotate models

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,28 +1,38 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Role < ActiveRecord::Base
 
   has_and_belongs_to_many :users
 
-  ROLE_SUPER_ADMIN          = "super_admin"
-  ROLE_USER_ADMIN           = "user_admin"
-  ROLE_MASTER_DATA_ADMIN    = "master_admin"
+  ROLE_SUPER_ADMIN = "super_admin"
+  ROLE_USER_ADMIN = "user_admin"
+  ROLE_MASTER_DATA_ADMIN = "master_admin"
 
   ROLES = [
-    {
-      :role => ROLE_SUPER_ADMIN,
-      :name => "Super Admin",
-      :desc => "Allows access to all functionalities."
-    },
-    {
-      :role => ROLE_USER_ADMIN,
-      :name => "User Admin",
-      :desc => "Allows access to User Management related functionalities."
-    },
-    {
-      :role => ROLE_MASTER_DATA_ADMIN,
-      :name => "Master Data Admin",
-      :desc => "Allows access to Administration of Master Data."
-    }
-  ] 
+      {
+          :role => ROLE_SUPER_ADMIN,
+          :name => "Super Admin",
+          :desc => "Allows access to all functionalities."
+      },
+      {
+          :role => ROLE_USER_ADMIN,
+          :name => "User Admin",
+          :desc => "Allows access to User Management related functionalities."
+      },
+      {
+          :role => ROLE_MASTER_DATA_ADMIN,
+          :name => "Master Data Admin",
+          :desc => "Allows access to Administration of Master Data."
+      }
+  ]
 
   def self.init_roles!
     ROLES.each do |r|
@@ -36,7 +46,7 @@ class Role < ActiveRecord::Base
   end
 
   def self.find_meta(name)
-    ROLES.find {|e| e[:role] == name }
+    ROLES.find { |e| e[:role] == name }
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string(255)
+#  crm_user_id            :integer
+#  firstname              :string(255)
+#  lastname               :string(255)
+#  address                :string(3000)
+#  phone                  :string(255)
+#  mobile                 :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -11,6 +40,9 @@ class User < ActiveRecord::Base
   attr_accessible :firstname, :lastname, :address, :phone, :mobile
 
   has_and_belongs_to_many :roles
+
+  has_paper_trail
+
 
   def role_manager
     @role_manager ||= ::RoleManager.new(self)
