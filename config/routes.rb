7162a370d840c78c0a480c4f348e5faffae50586
Senthil Kumar <senthilkumar@kreatio.com>
2014-01-13 23:34:44 +0530

    Mounted rails admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PccMain::Application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
   devise_for :users, :controllers => { :registrations => 'registrations' }
 
   # Misc. static pages
