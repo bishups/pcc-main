d8b2e1aff14c911f119bb69c16f545d6cbd1d0f1
smriti <smritigarg87@gmail.com>
2014-02-23 16:43:38 +0530

    app

--- app/helpers/kit_schedules_helper.rb
+++ app/helpers/kit_schedules_helper.rb
@@ -1,2 +1,21 @@
-module KitSchedulesHelper
+module VenueSchedulesHelper
+  
+  def venue_schedule_state_tag(vs)
+    proc do
+      if vs.block_requested?
+        '<span class="label label-info">Blocked Requested</span>'
+      elsif vs.blocked?
+        '<span class="label label-info">Blocked</span>'
+      elsif vs.payment_pending?
+        '<span class="label label-warning">Payment Pending</span>'
+      elsif vs.paid?
+        '<span class="label label-success">Paid</span>'
+      elsif vs.cancelled?
+        '<span class="label label-danger">Cancelled</span>'
+      else
+        '<span class="label label-default">Unknown</span>'
+      end
+    end.call().html_safe
+  end
+
 end
