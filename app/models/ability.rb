4408736f36083b070e8e1f3232a2a058bb13ad12
Senthil Kumar <senthilkumar@kreatio.com>
2014-03-01 14:27:31 +0530

    Added to apply cancan rules for authorisation.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,37 +2,51 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    #user.access_privileges.each do |access_privilege|
+    #  can :manage, :all, :center_id => access_privilege.resource_id
+    #end
+    user.access_privileges.each do |access_privilege|
+    #  puts "Inside access_privilege loop   #{access_privilege.permissions.count} "
+      access_privilege.permissions.each do |permission|
+      #  puts "Inside access_privilege loop #{permission.subject},  #{access_privilege.resource} "
+        if access_privilege.resource and permission.subject
+          # Ensure that the subject class has a belongs_to assosiation so that it can be filetered based on that.
+          # For ex) If below rule has subject in permission as 'Venue' and resource allowed for the user is a particluat
+          # center then it will look like
+          #         can :manage, Venue, :center => @center ( Center the user is allowed to access )
+          # So ENSURE TO HAVE A BELONGS TO ASSOSATION TO CENTER OR ANY OTHER RESOURCE USED TO CONFIGURE IN ROLES FOR THE
+          # CLASS WHICH WE NEED CANCAN
+          subject_class = permission.subject.constantize
+          puts "######Inside permission loop #{ subject_class.reflections[:center] and subject_class.reflections[:center].macro == :belongs_to}  "
+          if subject_class.reflections[:center] and subject_class.reflections[:center].macro == :belongs_to
+            puts "Before can definition subject_class => #{subject_class} and center_id #{access_privilege.resource_id} "
+            can :manage, subject_class, {:center_id => access_privilege.resource_id}
+          elsif subject_class.reflections[:centers]
+            can :manage, subject_class, {:centers => {:id => resource}}
+          end
 
-    user ||= User.new
-    rm = user.role_manager
+          #do |subject|
+          #  if subject.respond_to?(:underscore)
+          #    :center_id => resource
+          #  else
+          #
+          #  end
+          #
+          #end
+          can :manage, permission.subject, access_privilege.resource_type.underscore => access_privilege.resource
+        elsif permission.subject and not access_privilege.resource
+          # If we want to provide access of all the Venues across all center then you don't need to provide
+          # resource type in the role
 
-    if rm.is_super_admin?
-      can :manage, :all
-    else
-      # None
+          can :manage, permission.subject.constantize
+
+        elsif user.is_super_admin?
+          # Super admin will have access to all the actions and models
+
+          can :manage, :all
+
+        end
+      end
     end
-
   end
 end
