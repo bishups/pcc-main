cf6544eb467496929890028a2c639704a481baa0
smriti <smritigarg87@gmail.com>
2014-03-16 16:13:25 +0530

    Dashboard Index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
-  
+    before_filter :authenticate_user!
+
   def index
 
     # @programs = current_user.programs
     # @venues = current_user.venues
     @kits = Kit.all
+    @venues = Venue.all
+    @teachers = Teacher.all
+    @programs = Program.all
     # @teacher = current_user.teachers
 
     @pills = ["Programs","Venues","Kits","Teachers"]
