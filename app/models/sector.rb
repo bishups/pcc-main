ef632c7bf32588f4b7a182cd3bd3dbf49b9156b0
Senthil Kumar <senthilkumar@kreatio.com>
2014-03-16 13:05:52 +0530

    Added the missing parts left while doing last merge.

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -18,4 +18,30 @@ class Sector < ActiveRecord::Base
 
   attr_accessible :name, :zone_id, :center_ids, :centers, :zone
 
+  rails_admin do
+    navigation_label 'Geo-graphical informations'
+    weight 1
+    list do
+      field :name
+      field :zone
+    end
+    edit do
+      field :name
+      field :zone  do
+        inverse_of  :sectors
+        inline_edit do
+          false
+        end
+        inline_add do
+          false
+        end
+      end
+      field :centers do
+        inline_add do
+          false
+        end
+      end
+    end
+  end
+
 end
