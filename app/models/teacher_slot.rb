08b46544aa3f05b5653d0c4e6ea0f6a98976d8f7
Amit Srivastava <amits8877@gmail.com>
2014-03-27 14:26:39 +0530

    not validating teacher slot for now

--- app/models/teacher_slot.rb
+++ app/models/teacher_slot.rb
@@ -8,13 +8,12 @@ class TeacherSlot < ActiveRecord::Base
   validates :status, :presence => true
   validates :user_id, :presence => true
 
-  validate :start_and_end_dates, :scheduleOverlapNotAllowed
+#  validate :start_and_end_dates, :scheduleOverlapNotAllowed
 
   # Validator
   def start_and_end_dates
-    if self.start_date and self.end_date
-      errors.add(:start_date, "must be in the future") if self.start_date < Time.now
-      errors.add(:end_date, "cannot be before start date") if self.start_date > self.end_date
+    if self.date
+      errors.add(:date, "- Start date must be in the future") if self.date < Time.now
     end
   end
 
