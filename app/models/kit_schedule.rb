c6bd2efffa3eb7369c6c9c5802216c3a83f207e2
anuj <anuj.ny@gmail.com>
2014-04-18 15:12:29 +0530

    Adding new schedule menu to Kit Schedules - to handle Reserve, overdue and under-repair for specific duration for a kit.

--- app/models/kit_schedule.rb
+++ app/models/kit_schedule.rb
@@ -17,35 +17,50 @@
 
 class KitSchedule < ActiveRecord::Base
 
+  STATE_AVAILABLE   = "Available"
+
+  STATE_RESERVED    = "Reserved"
+  STATE_UNDER_REPAIR = "Under Repair"
+  STATE_UNAVAILABLE_OVERDUE = "Overdue"
+
   STATE_BLOCKED     = "Blocked"
   STATE_ISSUED      = "Issued"
   STATE_ASSIGNED    = "Assigned"
   STATE_OVERDUE     = "Overdue"
+  STATE_RETURNED    = "Returned"
   STATE_CANCELLED  = "Cancelled"
-  STATE_CLOSED      = "Closed"
 
-  CONNECTED_STATES = [STATE_BLOCKED, STATE_ASSIGNED, STATE_ISSUED]
+  FINAL_STATES = [STATE_RETURNED, STATE_CANCELLED]
+  CONNECTED_STATES = [STATE_BLOCKED, STATE_ASSIGNED, STATE_ISSUED, STATE_OVERDUE]
+  RESERVED_STATES = [STATE_RESERVED, STATE_UNDER_REPAIR, STATE_UNAVAILABLE_OVERDUE]
+
+  EVENT_RESERVE    = "Reserve"
+  EVENT_UNDER_REPAIR = "Under Repair"
+  EVENT_UNAVAILABLE_OVERDUE = "Overdue"
 
   EVENT_BLOCK      = "Block"
   EVENT_ISSUE      = "Issue"
-  EVENT_ASSIGN     = "Assign"
   EVENT_OVERDUE    = "Overdue"
   EVENT_CANCEL     = "Cancel"
-  EVENT_CLOSE      = "Close"
+  EVENT_RETURNED   = "Returned"
+
+  NOTIFICATIONS = [EVENT_OVERDUE]
+  NON_MENU_EVENTS = [EVENT_BLOCK, EVENT_RESERVE, EVENT_UNDER_REPAIR, EVENT_UNAVAILABLE_OVERDUE]
+  PROCESSABLE_EVENTS = [EVENT_ISSUE, EVENT_RETURNED, EVENT_CANCEL]
 
   belongs_to :kit
   belongs_to :program
   belongs_to :issued_to_user, :class_name => User
   belongs_to :blocked_by_user, :class_name => User
 
-  attr_accessible :program_id, :kit_id,:end_date, :start_date,:state
+  attr_accessible :program_id, :kit_id,:end_date, :start_date, :state, :comments
 
   validates :start_date, :presence => true
   validates :end_date, :presence => true
   validates :kit_id , :presence => true
   validates :state , :presence => true
-  validates :program_id, :presence => true
-  validates_uniqueness_of :program_id, :scope => "kit_id"
+  validates :program_id, :presence => true, :unless => :kit_reserved?
+  validates_uniqueness_of :program_id, :scope => "kit_id", :unless => :kit_reserved?
 
   before_create :assign_start_date_end_date!#, :assign_person_ids!
   #after_create :connect_program!
@@ -53,27 +68,30 @@ class KitSchedule < ActiveRecord::Base
   #checking for overlap validation 
   validates_with KitScheduleValidator
 
-  PROCESSABLE_EVENTS = [
-      EVENT_BLOCK, EVENT_ASSIGN, EVENT_ISSUE, EVENT_OVERDUE, EVENT_CLOSE, EVENT_CANCEL
-  ]
 
   
   def initialize(*args)
     super(*args)
   end
    
-  state_machine :state , :initial => STATE_BLOCKED do
+  state_machine :state , :initial => STATE_AVAILABLE do
 
-    event EVENT_ASSIGN do
-      transition [STATE_BLOCKED] => STATE_ASSIGNED, :if => :canChangeState?
+    # TODO - clean up the kit schedule validations
+    # TODO - see how to split effectively into two state machines ?
+    event EVENT_BLOCK do
+      transition [STATE_AVAILABLE] => STATE_BLOCKED
     end
-    
+
+    event ::Program::ANNOUNCED do
+      transition STATE_BLOCKED => STATE_ASSIGNED
+    end
+
     event EVENT_ISSUE do
-      transition [STATE_ASSIGNED] => STATE_ISSUED, :if => :canChangeState?
+      transition [STATE_ASSIGNED] => STATE_ISSUED
     end
     
-    event EVENT_CLOSE do
-      transition [STATE_ISSUED] => STATE_CLOSED , :if => :canChangeState?
+    event EVENT_RETURNED do
+      transition [STATE_OVERDUE, STATE_ISSUED] => STATE_RETURNED , :if => :canChangeState?
     end
     
     event EVENT_OVERDUE do
@@ -81,12 +99,46 @@ class KitSchedule < ActiveRecord::Base
     end
 
     event EVENT_CANCEL do
-      transition [STATE_BLOCKED, STATE_ASSIGNED] => STATE_CANCELLED
+      transition [STATE_BLOCKED] => STATE_CANCELLED
     end
+
+    event ::Program::DROPPED do
+      transition [STATE_BLOCKED] => STATE_CANCELLED
+    end
+
+    event ::Program::CANCELLED do
+      transition [STATE_ASSIGNED] => STATE_CANCELLED
+    end
+
+    event EVENT_RESERVE do
+      transition [STATE_AVAILABLE] => STATE_RESERVED
+    end
+
+    event EVENT_UNDER_REPAIR do
+      transition [STATE_AVAILABLE] => STATE_UNDER_REPAIR
+    end
+
+    event EVENT_UNAVAILABLE_OVERDUE do
+      transition [STATE_AVAILABLE] => STATE_UNAVAILABLE_OVERDUE
+    end
+    before_transition STATE_AVAILABLE => [STATE_RESERVED, STATE_UNDER_REPAIR, STATE_UNAVAILABLE_OVERDUE], :do => :reserve_fields_present?
+
+  end
+
+  def kit_reserved?
+    RESERVED_STATES.include?(self.state)
+  end
+
+  def reserve_fields_present?
+    if self.comments.blank?
+      self.errors[:comments] << " cannot be blank."
+      return false
+    end
+    true
   end
 
   def is_connected?
-    CONNECTED_STATES.include?(self.state)
+    self.program_id && CONNECTED_STATES.include?(self.state)
   end
 
   def set_up_details!
