a347497406f43e35c1f96480c6a43f1904da1ece
Senthil Kumar <senthilkumar@kreatio.com>
2014-04-21 20:23:32 +0530

    Added scope for "by_role" which will b used for fetching access privilleges based on a role name and this will be used for getting centers for a particular role of a user.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,7 +3,9 @@ RailsAdmin.config do |config|
   config.main_app_name = Proc.new { |controller| [ "PCC", "Administration BackOffice - #{controller.params[:action].try(:titleize)}" ] }
   #config.included_models = [Kit, KitItem, User, Zone, Sector, Center, ProgramType, Venue, Permission, Role]
   config.included_models = [AccessPrivilege, Timing, Kit, KitItem, KitItemName, Teacher, User, Zone, Sector, Venue, Center, ProgramType, Permission, Role, Pincode, Comment]
-  #config.authorize_with :cancan
+  #config.authorize_with do |controller|
+  #  redirect_to main_app.root_path unless current_user.is_super_admin?
+  #end
   config.audit_with :history, User
   config.actions do
     # root actions
