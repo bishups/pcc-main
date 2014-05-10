4fe731e12a9a5329c8630201e04413defc9f9f7e
Senthil Kumar <senthilkumar@kreatio.com>
2014-05-10 22:23:02 +0530

    Changed rules in admin_ability to make Zones and Sector editable

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -11,6 +11,8 @@ class AdminAbility
     else
       can :read, User, {:id => user.accessible_centers.map(&:user_ids).flatten.uniq}
       can :read, Center, {:id => user.accessible_centers}
+      can :read, Pincode
+
 
       if user.is?(:kit_coordinator)
         can :manage, Kit, {:centers => {:id => user.accessible_centers(User::ROLE_ACCESS_HIERARCHY[:kit_coordinator][:text]).map(&:id).uniq}}
@@ -28,8 +30,7 @@ class AdminAbility
 
       if user.is?(:sector_coordinator)
         can [:read,:update], [User], {:id => user.accessible_centers.map(&:user_ids).flatten.uniq}
-        can :manage, Sector, {:id => user.accessible_sectors}
-        cannot [:create, :destroy], Sector
+        can  :manage ,Sector, {:id => user.accessible_sectors.map(&:id)}
         can :manage, Center, {:id => user.accessible_centers(User::ROLE_ACCESS_HIERARCHY[:sector_coordinator][:text]).map(&:id).uniq}
         can :manage, AccessPrivilege, {:resource_type => "Center", :resource_id => user.accessible_centers}
         cannot :destroy, AccessPrivilege, { :role_id => Role.where(:name=>User::ROLE_ACCESS_HIERARCHY[:teacher][:text]).first.id }
@@ -37,8 +38,7 @@ class AdminAbility
       end
 
       if user.is?(:zonal_coordinator) or user.is?(:zao)
-        can :manage, Zone,{:id => user.accessible_zones}
-        cannot [:create, :destroy], [Zone]
+        can :manage, Zone, {:id => user.accessible_zones.map(&:id) }
       end
 
 
