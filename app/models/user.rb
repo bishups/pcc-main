e5984bcc2567859af6235eed168f056035feba3f
Senthil Kumar <senthilkumar@kreatio.com>
2014-05-10 22:26:07 +0530

    Stripped the approver email before finding it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -117,7 +117,7 @@ class User < ActiveRecord::Base
   end
 
   def validate_approver_email
-    approver = User.where(:email => self.approver_email).first
+    approver = User.where(:email => self.approver_email.strip).first
     unless approver and (approver.is?(:super_admin) or  approver.is?(:zonal_coordinator) or approver.is?(:sector_coordinator))
       errors[:approver_email] << "is not valid. Either Email is in-correct or the provided email is not of a approver."
     end
