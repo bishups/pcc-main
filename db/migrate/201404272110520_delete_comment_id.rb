e43080ce687607bc0d1372c5b5fee85fc707c2cd
abhisek <abhisek.datta@gmail.com>
2014-05-18 11:41:25 +0530

    Fixed typo in migration
    Signed-off-by: abhisek <abhisek.datta@gmail.com>

--- db/migrate/201404272110520_delete_comment_id.rb
+++ db/migrate/201404272110520_delete_comment_id.rb
@@ -1,6 +1,6 @@
 class DeleteCommentId < ActiveRecord::Migration
   def change
-    remove_column :kit_schedules, :comment_id, :due_date
+    remove_column :kit_schedules, :comment_id
     remove_column :kits, :comment_id, :requester_id
     remove_column :programs, :comment_id
     remove_column :teacher_schedules, :comment_id, :teacher_comments
