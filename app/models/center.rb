03b21261efd483df733f0eaf003e9ac40cbf11d9
senthil-isha <senthilkumar.rec83@gmail.com>
2014-06-23 13:27:48 +0530

    Fixed the pincode display issue. Already used pincodes are not shown in the pincode list.

--- app/models/center.rb
+++ app/models/center.rb
@@ -27,9 +27,7 @@ class Center < ActiveRecord::Base
   has_and_belongs_to_many :program_donations
   attr_accessible :program_donations, :program_donation_ids
 
-  validates :name,:sector, :presence => true
-
-
+  validates :name, :sector, :presence => true
 
   rails_admin do
     navigation_label 'Geo-graphical informations'
@@ -55,8 +53,19 @@ class Center < ActiveRecord::Base
         inline_add false
       end
       field :pincodes do
+        help " Only Pincodes which are not already used by any Center are listed above. If your Pincode is missing, please contact Help desk."
         inline_add do
           false
+        end
+        associated_collection_cache_all true # REQUIRED if you want to SORT the list as below
+        associated_collection_scope do
+          # bindings[:object] & bindings[:controller] are available, but not in scope's block!
+          Proc.new { |scope|
+            # scoping all Players currently, let's limit them to the team's league
+            # Be sure to limit if there are a lot of Players and order them by position
+            # scope = scope.where(:id => accessible_centers )
+            scope = Pincode.unused
+          }
         end
       end
       field :program_donations do
--- app/models/pincode.rb
+++ app/models/pincode.rb
@@ -1,12 +1,13 @@
 class Pincode < ActiveRecord::Base
   belongs_to :center
   attr_accessible :location_name, :pincode, :center, :center_id
-  validates :center, :presence => true
+#  validates :center, :presence => true
   validates :location_name, :presence => true, :uniqueness => true
   validates :pincode,  :presence => true, :length => { is: 6}, :numericality => {:only_integer => true }
 
   validates_uniqueness_of :pincode, :scope => :deleted_at
 
+  scope :unused, where({:center_id => nil})
 
   acts_as_paranoid
 
