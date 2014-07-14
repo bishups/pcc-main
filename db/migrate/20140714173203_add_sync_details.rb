e2f0b16701a907151d9f2571dab62c7b36a9c5eb
senthil-isha <senthilkumar.rec83@gmail.com>
2014-07-14 23:11:16 +0530

    Added Sync_ts and changed sync_id to string

---
+++ db/migrate/20140714173203_add_sync_details.rb
@@ -0,0 +1,39 @@
+class AddSyncDetails < ActiveRecord::Migration
+    def change
+      add_column :zones, :sync_ts, :string
+      remove_column :zones, :sync_id
+      add_column :zones, :sync_id, :string
+
+      add_column :sectors, :sync_ts, :string
+      remove_column :sectors, :sync_id
+      add_column :sectors, :sync_id, :string
+
+      add_column :centers, :sync_ts, :string
+      remove_column :centers, :sync_id
+      add_column :centers, :sync_id, :string
+
+      add_column :users, :sync_ts, :string
+      remove_column :users, :sync_id
+      add_column :users, :sync_id, :string
+
+      add_column :roles_users, :sync_ts, :string
+      remove_column :roles_users, :sync_id
+      add_column :roles_users, :sync_id, :string
+
+      add_column :access_privileges, :sync_ts, :string
+      remove_column :access_privileges, :sync_id
+      add_column :access_privileges, :sync_id, :string
+
+      add_column :programs, :sync_ts, :string
+      remove_column :programs, :sync_id
+      add_column :programs, :sync_id, :string
+
+      add_column :program_types, :sync_ts, :string
+      remove_column :program_types, :sync_id
+      add_column :program_types, :sync_id, :string
+
+      add_column :program_donations, :sync_ts, :string
+      remove_column :program_donations, :sync_id
+      add_column :program_donations, :sync_id, :string
+    end
+end
