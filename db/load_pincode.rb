73399a2af74ece65dbc5e8413928d1abbf6a5334
senthil-isha <senthilkumar.rec83@gmail.com>
2014-09-24 18:43:50 +0530

    hanged in local

--- db/load_pincode.rb
+++ db/load_pincode.rb
@@ -11,3 +11,21 @@ sheet[:table].each do |row|
   end
 
 end
+
+
+timing_mapping = {
+
+   "Morning (6am-9am)" => {:name => "Morning (6am-10am)", :start_time => "6am", :end_time => "10am" },
+   "Afternoon (10am-1pm)" => {:name => "Afternoon (10am-2pm)", :start_time => "10am", :end_time => "2pm"},
+   "Evening (2pm-5pm)" => {:name => "Evening (2pm-6pm)", :start_time => "2pm", :end_time => "6pm"},
+   "Night (6pm-9pm)" => {:name => "Night (6pm-10pm)", :start_time => "6pm", :end_time => "10pm"}
+
+}
+
+timing_mapping.each do |old_name,new_timing|
+  t=Timing.find_or_initialize_by_name(old_name)
+  t.attributes=(new_timing)
+  if not t.save
+    puts "Timing #{t.name} has not been saved because of #{t.errors.messages}"
+  end
+end
