dddde82d9d418407806a782d13eaf7a4d0b51f1a
senthil-isha <senthilkumar.rec83@gmail.com>
2014-11-24 20:43:09 +0530

    Added for mapping new timing value

--- db/map_timings.rb
+++ db/map_timings.rb
@@ -1,16 +1,17 @@
-
 timing_mapping = {
-    "Morning (6am-9am)" => ["Morning (6am-10am)", "2000-01-01 06:00", "2000-01-01 10:00" ],
-    "Afternoon (10am-1pm)" => [ "Afternoon (10am-2pm)", "2000-01-01 10:00", "2000-01-01 14:00" ],
-    "Evening (2pm-5pm)" => ["Evening (2pm-6pm)","2000-01-01 14:00","2000-01-01 18:00"],
-    "Night (6pm-9pm)" => ["Night (6pm-10pm)", "2000-01-01 18:00", "2000-01-01 22:00"]
+    "Morning (6am-9am)"    => ["Morning (6am-10am)", "2000-01-01 06:00", "2000-01-01 10:00"],
+    "Afternoon (10am-1pm)" => ["Afternoon (10am-2pm)", "2000-01-01 10:00", "2000-01-01 14:00"],
+    "Evening (2pm-5pm)"    => ["Evening (2pm-6pm)", "2000-01-01 14:00", "2000-01-01 18:00"],
+    "Night (6pm-9pm)"      => ["Night (6pm-10pm)", "2000-01-01 18:00", "2000-01-01 22:00"]
 }
 
-timing_mapping.each do |old_timing,new_timing|
+timing_mapping.each do |old_timing, new_timing|
   t=Timing.find_by_name(old_timing)
   puts "Process timing #{t.name}" if t
-  t.name = new_timing[0]
+  t.name       = new_timing[0]
   t.start_time = new_timing[1]
-  t.end_time = new_timing[2]
-  t.save
+  t.end_time   = new_timing[2]
+  unless t.save
+    put "#{t.errors.messages}"
+  end
 end
