5d293f45bb79dfe47d720c0e09d1575443c85bab
Ubuntu <ubuntu@ip-172-31-0-124.ap-southeast-1.compute.internal>
2015-01-26 11:40:20 +0000

    Moved the mapping of program date timings to seperate file and moved last in migration

--- db/migrate/20141114010890_create_programs_date_timings.rb
+++ db/migrate/20141114010890_create_programs_date_timings.rb
@@ -4,26 +4,26 @@ class CreateProgramsDateTimings < ActiveRecord::Migration
       t.belongs_to  :program
       t.belongs_to  :date_timing
     end
-    Program.all.each do |program|
-      day_timing_ids = []
-      for i in 1..program_type.no_of_days
-        day_timing_ids << program.timing_ids
-      end
-      date_timings = []
-      day_offset = 0
-      day_timing_ids.each { |dt|
-        date = program.start_date.to_date + day_offset.day
-        dt.each { |t|
-          # double check - create only if timing_id is there
-          next if t.blank?
-          date_timings << DateTiming.where(:date => date, :timing_id => t).first_or_create
-          puts "program # #{program.id}:: adding date_time :: #{date - timing_id}"
-        }
-        day_offset = day_offset + 1
-      }
-      # update data timings
-      program.update_attributes :date_timings => date_timings
-    end
+#    Program.all.each do |program|
+#      day_timing_ids = []
+#      for i in 1..program.program_donation.program_type.no_of_days
+#        day_timing_ids << program.timing_ids
+#      end
+#      date_timings = []
+#      day_offset = 0
+#      day_timing_ids.each { |dt|
+#        date = program.start_date.to_date + day_offset.day
+#        dt.each { |t|
+#         # double check - create only if timing_id is there
+#          next if t.blank?
+#          date_timings << DateTiming.where(:date => date, :timing_id => t).first_or_create
+#          puts "program # #{program.id}:: adding date_time :: #{date} - #{t}"
+#        }
+#        day_offset = day_offset + 1
+#      }
+#      # update data timings
+#      program.update_attributes :date_timings => date_timings
+#    end
   end
   def down
   end
---
+++ db/migrate/20150126100346_map_program_date_timings.rb
@@ -0,0 +1,27 @@
+class MapProgramDateTimings < ActiveRecord::Migration
+  def up
+      Program.all.each do |program|
+      day_timing_ids = []
+      for i in 1..program.program_donation.program_type.no_of_days
+        day_timing_ids << program.timing_ids
+      end
+      date_timings = []
+      day_offset = 0
+      day_timing_ids.each { |dt|
+        date = program.start_date.to_date + day_offset.day
+        dt.each { |t|
+          # double check - create only if timing_id is there
+          next if t.blank?
+          date_timings << DateTiming.where(:date => date, :timing_id => t).first_or_create
+          puts "program # #{program.id}:: adding date_time :: #{date} - #{t}"
+        }
+        day_offset = day_offset + 1
+      }
+      # update data timings
+      program.update_attributes :date_timings => date_timings
+     end
+  end
+
+  def down
+  end
+end
